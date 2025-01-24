b486313a9f5fa1f68d296e3c9cb7214fcd03476a
neha gusain <nehagusain4134@gmail.com>
2025-01-24 18:41:35 +0530

    last commit

--- app.js
+++ app.js
@@ -1,6 +1,60 @@
-var clutter = "";
-for (var i = 0; i <= 101; i++) {
-  var rn= Math.floor(Math.random()*10);
-    clutter += `<div class="bubble">${rn}</div>`; // Using dynamic content
+var score = 0;
+var hitrn = 0;
+
+// Event bubbling: when you click on an element, the event will be raised on that element, 
+// and if the event is not found, it will look for a listener on the parent element.
+function increasescore() {
+  score += 10;
+  document.querySelector("#score").textContent = score;
+}                     
+
+function getnewhit() {
+  hitrn = Math.floor(Math.random() * 10);
+  document.querySelector("#hit").textContent = hitrn;
 }
-document.querySelector("#pbtm").innerHTML = clutter;
+
+function makebubble() {
+  var clutter = "";
+  for (var i = 0; i <= 101; i++) {
+    var rn = Math.floor(Math.random() * 10);
+    clutter += `<div class="bubble">${rn}</div>`; // Using dynamic content
+  }
+  document.querySelector("#pbtm").innerHTML = clutter;
+}
+
+var timer = 30;
+function runtimer() {
+  var timerinter = setInterval(function() {
+    if (timer > 0) {
+      timer--;
+      document.querySelector("#timer").textContent = timer;
+    } else {
+      clearInterval(timerinter);
+      document.querySelector("#pbtm").innerHTML=`<h1>Game Over</h1>`;
+    }
+  }, 1000);
+}
+
+document.querySelector("#pbtm")
+.addEventListener("click", function(dets) {
+  var clicked =Number(dets.target.textContent);
+  if(clicked ===hitrn){
+    increasescore();
+    makebubble();
+    getnewhit();
+
+  }
+  }
+);
+
+getnewhit();
+runtimer();
+makebubble();
+
+
+
+//var clicked = Number(dets.target.textContent);
+//if (clicked == hitrn) {
+  //increasescore();
+  //makebubble();
+  //getnewhit();
